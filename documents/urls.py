5afa0496166c5916786ee51bbad04e57936178c9
Sunil Sahu <sunil.sahu.w.15@gmail.com>
2026-09-07 11:45:37 +0530

    Add document view route

--- documents/urls.py
+++ documents/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import DocumentViewSet
+from .file_views import view_document
 
 router = DefaultRouter()
 
@@ -12,6 +13,11 @@ router.register(
 )
 
 urlpatterns = [
+    path(
+        "<int:pk>/view/",
+        view_document,
+        name="document-view",
+    ),
     path(
         "",
         include(router.urls),
